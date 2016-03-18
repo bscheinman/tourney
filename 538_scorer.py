69996f4de3c0618788f80426685d449583bbeca7
Brendon Scheinman <bscheinman@gmail.com>
2016-03-17 22:52:03 -0400

    Added scoring script for 538 data

---
+++ 538_scorer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+from decimal import Decimal
+from gzip import GzipFile
+import json
+import pycurl
+from StringIO import StringIO
+import urllib2
+
+ROUND_SCORES = [0, 1, 1, 2, 2, 2, 3]
+ENDPOINT = 'https://projects.fivethirtyeight.com/march-madness-api/2016/latest.json'
+
+def score(data):
+    scores = {}
+    teams = data['forecasts']['mens']['teams']
+    for team in teams:
+        name = team['team_name']
+        score = Decimal(0)
+        for i in xrange(len(ROUND_SCORES)):
+            p = Decimal(team['rd{0}_win'.format(i + 1)])
+            #print(' '.join(map(str, (name, i, p))))
+            score += p * ROUND_SCORES[i]
+        scores[name] = score
+    return scores
+
+compressed_data = urllib2.urlopen(ENDPOINT).read()
+compressed_file = StringIO()
+compressed_file.write(compressed_data)
+compressed_file.seek(0)
+raw_file = GzipFile(fileobj=compressed_file, mode='rb')
+raw_data = raw_file.read()
+data = json.loads(raw_data)
+scores = score(data)
+scores = sorted(scores.iteritems(), key=lambda x: x[0])
+
+for team, score in scores:
+    if score:
+        print('{0}|{1}'.format(team, score))
