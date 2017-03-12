dd74520653a1528e944acceecb5e44942dfc1db5
Brendon Scheinman <bscheinman@gmail.com>
2017-03-12 12:26:39 -0700

    update bracket parser

--- get_data.py
+++ get_data.py
@@ -8,20 +8,20 @@ import urllib2
 RATINGS_URL = 'http://kenpom.com/'
 BRACKET_URL = 'http://espn.go.com/ncb/bracketology'
 
+NAME_CONVERSIONS = {}
+
 def get_bracket(out_file):
     html = urllib2.urlopen(BRACKET_URL).read()
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, 'html.parser')
     bracket = soup.find('div', { 'class': 'bracket' })
-    for link in bracket.find_all('a'):
-        team_name = link.string
-        replacement = NAME_CONVERSIONS.get(team_name.lower(), None)
-        if replacement:
-            team_name = replacement
-        out_file.write('{0}\n'.format(team_name))
+    for entry in bracket.find_all('div', {'class': 'team'}):
+        team_names = [link.string for link in entry.find_all('a')]
+        team_names = [NAME_CONVERSIONS.get(name.lower(), name) for name in team_names]
+        out_file.write('{0}\n'.format(','.join(team_names)))
 
 def get_ratings(out_file):
     html = urllib2.urlopen(RATINGS_URL).read()
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, 'html.parser')
     ratings = soup.find('table', { 'id': 'ratings-table' })
     ratings = ratings.tbody
     for row in ratings.find_all('tr'):
@@ -35,3 +35,5 @@ def get_ratings(out_file):
 if __name__ == '__main__':
     with open('ratings.txt', 'w') as ratings_file:
         get_ratings(ratings_file)
+    with open('bracket.txt', 'w') as bracket_file:
+        get_bracket(bracket_file)
